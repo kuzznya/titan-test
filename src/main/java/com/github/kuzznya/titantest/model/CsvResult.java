499280f15afd11905e2d9ce78b5fb5004ecf3fd8
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-23 02:44:28 +0300

    fix: Add space after comma in CSV string

--- src/main/java/com/github/kuzznya/titantest/model/CsvResult.java
+++ src/main/java/com/github/kuzznya/titantest/model/CsvResult.java
@@ -9,8 +9,8 @@ public interface CsvResult {
     default String getDataAsString() {
         StringBuilder builder = new StringBuilder();
         for (Object value : this.getData())
-            builder.append(",").append(value.toString());
-        builder.deleteCharAt(0);
+            builder.append(", ").append(value.toString());
+        builder.delete(0, 2);
         return builder.toString();
     }
 }
