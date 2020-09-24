dc5f856e4d8284c8e96a9b1150e2fde37c1feae4
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-25 00:57:27 +0300

    refactor: Minor changes

--- src/main/java/com/github/kuzznya/titantest/web/CalculationRouter.java
+++ src/main/java/com/github/kuzznya/titantest/web/CalculationRouter.java
@@ -1,15 +1,7 @@
 package com.github.kuzznya.titantest.web;
 
-import com.github.kuzznya.titantest.dto.CalculationRequest;
-import com.github.kuzznya.titantest.service.CalculationSeriesService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
-import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
