9dfdf96329c4b1273ca3992398e9d855178688aa
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-25 00:53:41 +0300

    feat: Improve OpenAPI documentation

--- src/main/java/com/github/kuzznya/titantest/web/CalculationHandler.java
+++ src/main/java/com/github/kuzznya/titantest/web/CalculationHandler.java
@@ -6,6 +6,11 @@ import com.github.kuzznya.titantest.exception.FunctionExecutionException;
 import com.github.kuzznya.titantest.model.OrderedCalculationResult;
 import com.github.kuzznya.titantest.model.UnorderedCalculationResult;
 import com.github.kuzznya.titantest.service.CalculationSeriesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -23,6 +28,30 @@ public class CalculationHandler {
         this.seriesService = seriesService;
     }
 
+    @Operation(
+            requestBody = @RequestBody(
+                    description = "Calculation params",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CalculationRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK - Calculation started",
+                            content = @Content(mediaType = "text/event-stream",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request - calculation cannot be started"
+                    )
+            },
+            description = "Start the calculation series & return data as soon as it is calculated"
+    )
     public Mono<ServerResponse> calculateUnordered(ServerRequest request) {
         Mono<CalculationRequest> bodyMono = request.bodyToMono(CalculationRequest.class);
         return bodyMono.flatMap(calculationRequest ->
@@ -43,6 +72,30 @@ public class CalculationHandler {
         );
     }
 
+    @Operation(
+            requestBody = @RequestBody(
+                    description = "Calculation params",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CalculationRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK - Calculation started",
+                            content = @Content(mediaType = "text/event-stream",
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request - calculation cannot be started"
+                    )
+            },
+            description = "Start the calculation series & format data by execution index (return two results at the same time)"
+    )
     public Mono<ServerResponse> calculateOrdered(ServerRequest request) {
         Mono<CalculationRequest> bodyMono = request.bodyToMono(CalculationRequest.class);
         return bodyMono.flatMap(calculationRequest ->
